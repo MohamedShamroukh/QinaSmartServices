ed1447b0e5d6916f44e250e8ae198c8bc38346dd
mohamed <mohamedshamroukh010@gmail.com>
2021-08-21 07:55:20 +0200

    9

--- QinaSmartMap/qinasmartmap/wsgi.py
+++ QinaSmartMap/qinasmartmap/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QinaSmartMap.settings.dev')
-os.environ["DJANGO_SETTINGS_MODULE"] = 'qinasmartmap.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qinasmartmap.settings.dev')
 application = get_wsgi_application()
 
