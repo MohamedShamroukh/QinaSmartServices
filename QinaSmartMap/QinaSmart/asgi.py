876cf53608444b19685651dfcf074464962134d1
mohamed <mohamedshamroukh010@gmail.com>
2021-08-21 15:25:40 +0200

    4

--- QinaSmartMap/QinaSmart/asgi.py
+++ QinaSmartMap/QinaSmart/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for qinasmartmap project.
+ASGI config for QinaSmart project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qinasmartmap.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QinaSmart.settings')
 
 application = get_asgi_application()
