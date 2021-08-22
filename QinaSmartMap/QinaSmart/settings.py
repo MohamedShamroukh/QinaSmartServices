98e6ee2d89549ce85e68de3b0e12dbda064847e7
mohamed <mohamedshamroukh010@gmail.com>
2021-08-22 15:00:58 +0200

    6

--- QinaSmartMap/QinaSmart/settings.py
+++ QinaSmartMap/QinaSmart/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/www.qinasmartservices.herokuapp.com/staticfiles'
 
 
 # STATIC_ROOT = "/var/www/ qinasmartservices.herokuapp.com/static/"
